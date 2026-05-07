b6285332964ded2184c5dd1984f58c2e3f3e6493
Reema21415 <445052064@std.psau.edu.sa>
2026-05-07 16:06:52 +0300

    Protect completed process counter with ReentrantLock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -61,7 +61,13 @@ class SharedResources {
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+        counterLock.lock();
+        try {
+            completedProcessCount++;
+        } finally {
+            counterLock.unlock();
+        }
+
     }
 
     // Method to add waiting time
